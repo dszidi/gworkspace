b2faabc0a9a3629af578d563f995ca4f12a54874
Damian Szidiropulosz <damianos@knoxbug.org>
2024-06-24 22:17:30 -0400

    Update path to GNUstep.sh

--- dubstep-install.sh
+++ dubstep-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -rf /System/Applications/GWorkspace.app
-. /System/Library/Makefiles/GNUstep.sh
+. /Developer/Makefiles/GNUstep.sh
 export GNUSTEP_INSTALLATION_DOMAIN=SYSTEM
 ./configure && gmake && gmake install
 
